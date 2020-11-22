5726349f06859f966a9cae8b8c5c44b6c6a7a17a
taro-sakakibara <theband0914@gmail.com>
2020-11-22 11:04:07 +0900

    新規登録機能の実装2
    モデルのバリデーションの設定
    エラーメッセージの表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name_kanji, :family_name_kanji, :first_name_kana, :family_name_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name_kanji, :family_name_kanji, :first_name_kana, :family_name_kana, :birthday,])
   end
 end
