1f9a44cc80f090facb1fbc7cd88cdc37b8fa42f4
taro-sakakibara <theband0914@gmail.com>
2020-11-23 18:29:20 +0900

    アクティブストレージの導入
    そのほか実装
    ・新規投稿ページへの遷移
    ・ストロングパラメーター

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :explanatory_text, :category_id,:condition_id, :delivery_fee_id, :prefecture_id, :shipping_days_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
