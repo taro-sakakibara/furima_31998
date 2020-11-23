a0807e8739927360b94185f9f4100952b3489be4
taro-sakakibara <theband0914@gmail.com>
2020-11-23 17:56:17 +0900

    アソシエーション、バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :explanatory_text
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :shipping_days_id
+    validates :price
+  end
+  
+  belongs_to :user
+  has_one :order
 end
