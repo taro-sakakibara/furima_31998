99a1a1a048033ba71e94d81fd6da575bc565e0fc
taro-sakakibara <theband0914@gmail.com>
2020-11-23 18:56:22 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,13 @@
 class Item < ApplicationRecord
   with_options presence: true do
-    validates :name
-    validates :explanatory_text
+    validates :name, length: { maximum: 40 }
+    validates :explanatory_text, length: { maximum: 1000 }
     validates :category_id
     validates :condition_id
     validates :delivery_fee_id
     validates :prefecture_id
     validates :shipping_days_id
-    validates :price
+    validates :price, format: {with: /\A[0-9]+\z/, message:"Full-width number characters"}
   end
   
   belongs_to :user
