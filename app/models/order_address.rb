0b5b3719ff5a50ddcd4b70c24006ef3d2da16cc4
taro-sakakibara <theband0914@gmail.com>
2020-11-27 17:37:44 +0900

    order_addressモデルのバリデーション /アソシエーションの設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,2 +1,11 @@
 class OrderAddress < ApplicationRecord
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/}
+    validates :prefecture_id
+    validates :municipalitiesformat: { with: /\A\d{11}\z/}
+    validates :address
+    validates :phone_number
+
+    belongs_to :order
+  end
 end
