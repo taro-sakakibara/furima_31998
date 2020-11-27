999dc0d8e16d30ca61e58f4786a5ff2bc24b3094
taro-sakakibara <theband0914@gmail.com>
2020-11-27 17:26:14 +0900

    order_addressesテーブルの設定

--- db/migrate/20201127081610_create_order_addresses.rb
+++ db/migrate/20201127081610_create_order_addresses.rb
@@ -1,6 +1,13 @@
 class CreateOrderAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :order_addresses do |t|
+      t.string :postal_code, null: false
+      t.integer :prefecture_id, null: false
+      t.string :municipalities	, null: false
+      t.string :address, null: false
+      t.string :building_name
+      t.string :phone_number, null: false
+      t.references :order, foreign_key: true
 
       t.timestamps
     end
