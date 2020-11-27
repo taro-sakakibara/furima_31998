cb27ea96925cea62b99fe1c0340c1e940c4486e7
taro-sakakibara <theband0914@gmail.com>
2020-11-27 12:24:18 +0900

    orders-controllerのアクション定義

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,9 @@
 const pay = () => {
-  console.log("カード情報トークン化のためのJavaScript");
+  const form = document.getElementById("credit-card-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("フォーム送信時にイベント発火")
+  });
 };
 
 window.addEventListener("load", pay);
