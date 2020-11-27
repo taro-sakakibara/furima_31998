f7c6e809bae599f45d8df7335dd998181cc74a54
taro-sakakibara <theband0914@gmail.com>
2020-11-27 19:36:44 +0900

    諸々の修正1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,10 +3,13 @@ class OrdersController < ApplicationController
     @order = Order.new
   end
   def create
-    @order = Order.new(order_params)
-    if @order.valid?
+    binding.pry
+    order = Order.create(order_params)
+    Order_address.create(order_address_params(order))
+
+    if order.valid?
       pay_item
-      @order.save
+      order.save
       return redirect_to root_path
     else
       render 'index'
@@ -16,13 +19,17 @@ end
   private
 
   def order_params
-    params.require(:order).merge(token: params[:token])
+    params.merge(token: params[:token])
+  end
+
+  def order_address(order)
+    params.permit(:postal_code, :prefecture_id, :municipalities, :address, :building_name, :phone_number).merge(order_id: order.id)
   end
 
   def pay_item
     Payjp.api_key = "sk_test_15c43d9f7b9d937f2f9ad981"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
     Payjp::Charge.create(
-      amount: @order.item.price,  # 商品の値段
+      amount: order.item.price,  # 商品の値段
       card: order_params[:token],    # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,33 +1,34 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_b6ffa0fbefe6f2e7541d48c0");
-  const form = document.getElementById("credit-card-form");
+  Payjp.setPublicKey("pk_test_b6ffa0fbefe6f2e7541d48c0"); // PAY.JPテスト公開鍵
+  const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const formResult = document.getElementById("credit-card-form");
+
+    const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("order[number]"),
-      cvc: formData.get("order[cvc]"),
-      exp_month: formData.get("order[exp_month]"),
-      exp_year: `20${formData.get("order[exp_year]")}`,
-  };
+      number: formData.get("number"),
+      cvc: formData.get("cvc"),
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
+    };
 
-  Payjp.createToken(card, (status, response) => {
-    if (status == 200) {
-      const token = response.id;
-      const renderDom = document.getElementById("credit-card-form");
-      const tokenObj = `<input value=${token} name='token' type="hidden"> `;
-      renderDom.insertAdjacentHTML("beforeend", tokenObj);
-    }
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
 
-    document.getElementById("order_number").removeAttribute("name");
-    document.getElementById("order_cvc").removeAttribute("name");
-    document.getElementById("order_exp_month").removeAttribute("name");
-    document.getElementById("order_exp_year").removeAttribute("name");
+      document.getElementById("card-number").removeAttribute("number");
+      document.getElementById("card-cvc").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
 
-    document.getElementById("charge-form").submit();
-  });
+      document.getElementById("charge-form").submit();
+    });
   });
 };
 
